82d713e639a6411fde17c339d1ca2f776b9bfc36
Reid Giles <33459088+ReidGiles@users.noreply.github.com>
2020-04-17 11:11:43 +0100

    Final

---
+++ BusinessSelfAssessmentSurvey/UnitTests/APITests.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BusinessSelfAssessmentSurvey.Models;
+using BusinessSelfAssessmentSurvey.Controllers;
+using System.Net.Http;
+using System.Net;
+
+namespace UnitTests
+{
+    /// <summary>
+    /// Miserably incomplete. Templated with how I would approach back end unit tests with more time.
+    /// </summary>
+    [TestClass]
+    public class APITests
+    {
+        [TestMethod]
+        public void GetWithValidID()
+        {
+            // Arange
+            // Expected results
+
+            // Act
+            // API calls
+
+            // Assert
+            // Expected results vs API response
+        }
+
+        [TestMethod]
+        public void GetWithIInvalidID()
+        {
+            // Arange
+            // Expected results
+
+            // Act
+            // API calls
+
+            // Assert
+            // Expected results vs API response
+        }
+
+        [TestMethod]
+        public void PostWithValidModel()
+        {
+            // Arange
+            // Expected results
+
+            // Act
+            // API calls
+
+            // Assert
+            // Expected results vs API response
+        }
+
+        [TestMethod]
+        public void PostWithInvalidModel()
+        {
+            // Arange
+            // Expected results
+
+            // Act
+            // API calls
+
+            // Assert
+            // Expected results vs API response
+        }
+    }
+}
---
+++ BusinessSelfAssessmentSurvey/UnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTests")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("db28bad2-8534-47b3-8485-9d3e7a17b094")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
